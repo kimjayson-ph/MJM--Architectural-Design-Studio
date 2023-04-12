823b91973342028bfb224887b957749955134f82
kimjayson-ph <kimjayson.ph@gmail.com>
2023-04-12 21:42:25 +0800

    rollback page

--- products.js
+++ products.js
@@ -1,39 +1,39 @@
 const products = [
   {
     id: 0,
-    name: "PRODUCTIVITY PLANNER",
-    price: 550.00,
+    name: "Product 1",
+    price: 100,
     instock: 20,
     description:
-      "This digital planner has 15 unique templates to help you assess, set goals, follow routines, track habits, and plan efficiently. With daily, weekly and monthly planning templates, this planner is what you need to hit that optimal level of productivity!",
-    imgSrc: "./main_assets/productivity.png",
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./main_assets/sample products.jpg",
   },
   {
     id: 1,
-    name: "FINANCIAL PLANNER",
-    price: 700.00,
+    name: "Product 2",
+    price: 150,
     instock: 20,
     description:
-      "This digital planner is perfect for staying organized and taking control of your money. With over 20 templates, this planner will help you keep track of your spending, budgeting, expenses, and income, so that you master your money.",
-    imgSrc: "./main_assets/financial.png",
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./main_assets/digital-planner.jpg",
   },
   {
     id: 2,
-    name: "FITNESS PLANNER",
-    price: 700.00,
+    name: "Product 3",
+    price: 200,
     instock: 20,
     description:
-      "With over 15 templates, this planner will help you level up your fitness game as you organize your fitness goals, perfect that schedule, and keep track of your progress and measurements.",
-    imgSrc: "./main_assets/fitness journal.png",
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./main_assets/sample products.jpg",
   },
   {
     id: 3,
-    name: "READING JOURNAL",
-    price: 400.00,
+    name: "Product 4",
+    price: 200,
     instock: 20,
     description:
-      "With 12 unique templates, this journal is designed with the book lover in mind. Set reading goals, keep track of your favorite books, make notes of what books to read, and level up your reading with our book challenges!",
-    imgSrc: "./main_assets/reading journal.png",
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./main_assets/digital-planner.jpg",
   },
   {
     id: 4,
