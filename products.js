bc87fc2e3126db8eb2751ae59aed682fa795da49
muningluning <121824167+muningluning@users.noreply.github.com>
2023-04-15 00:30:01 +0800

    Add files via upload
    update_digi products 04152023

--- products.js
+++ products.js
@@ -7,6 +7,11 @@ const products = [
     description:
       "This planner is designed to help you accelerate reaching your goals in a 3-month actionable print!",
     imgSrc: "./products_page/dailygoal_planner.png",
+    productivity1: "./products_page/pro (1).png",
+    productivity2: "./products_page/pro (2).png",
+    productivity3: "./products_page/pro (3).png",
+    detail:
+      "Has over 15 creative templates to help you assess, set goals, follow routines, track habits, and plan efficiently. Once purchased, you'll receive: An email with a link to download your purchase. This will include 1 .ZIP file which includes 3 .PDF files: (1) Instructions and Installation Guide, (2) Printable Productive Planner, (3)Linked Digital Productive Planner. Please note that THIS PRODUCT IS DIGITAL IN NATURE, NO PHYSICAL PRODUCT WILL BE SHIPPED.",
   },
   {
     id: 1,
@@ -16,6 +21,11 @@ const products = [
     description:
       "This digital planner is your comrade in arms in organizing and taking control of your money, from recurring bills to paying down debt!",
     imgSrc: "./products_page/financial_planner.png",
+    productivity1:"./products_page/finance (1).png",
+    productivity2:"./products_page/finance (2).png",
+    productivity3:"./products_page/finance (3).png",
+    detail:
+    "Has over 20 creative templates, this planner will help keep track of your spending, budgeting, expenses, and income so you can master your money. From goal-setting to debt payment to net worth tracking! Once purchased, you'll receive: An email with a link to download your purchase. This will include 1 .ZIP file which includes 3 .PDF files: (1) Instructions and Installation Guide, (2) Printable Productive Planner, (3)Linked Digital Productive Planner. Please note that THIS PRODUCT IS DIGITAL IN NATURE, NO PHYSICAL PRODUCT WILL BE SHIPPED.",
   },
   {
     id: 2,
@@ -25,32 +35,40 @@ const products = [
     description:
       "This planner will help you level up your fitness game as you organize your fitness goals, perfect that workout schedules and track your progress and measurements!",
     imgSrc: "./products_page/fitness_planner.png",
+    productivity1:"./products_page/fitness (1).png",
+    productivity2:"./products_page/fitness (2).png",
+    productivity3:"./products_page/fitness (4).png",
+    detail:
+      "With 15 unique templates, this digital planner will help organize your fitness goals, perfect your workout schedule, and keep track of your progress and measurements. Once purchased, you'll receive: An email with a link to download your purchase. This will include 1 .ZIP file which includes 3 .PDF files: (1) Instructions and Installation Guide, (2) Printable Productive Planner, (3)Linked Digital Productive Planner. Please note that THIS PRODUCT IS DIGITAL IN NATURE, NO PHYSICAL PRODUCT WILL BE SHIPPED.",
   },
   {
     id: 3,
-    name: "HAPPY HOME PLANNER",
-    price: 200,
-    instock: 20,
-    description:
-      "This digital planner is perfect in keeping your cleaning schedules, household meals, and household bills organized!",
-    imgSrc: "./products_page/home_planner.png",
-  },
-  {
-    id: 4,
     name: "NUTRITION PLANNER",
     price: 200,
     instock: 20,
     description:
       "From planning your weekly meals to logging your favorite recipes, we're helping you stay organized and maintain a healthy and balanced diet!",
     imgSrc: "./products_page/nutrition_planner.png",
+    productivity1:"./products_page/nutrition (1).png",
+    productivity2:"./products_page/nutrition (2).png",
+    productivity3:"./products_page/nutrition (3).png",
+    detail:
+    "With over 10 unique templates, this digital planner will help you create a weekly meal prep menu, organize your grocery list, track your macros - and so much more! Once purchased, you'll receive: An email with a link to download your purchase. This will include 1 .ZIP file which includes 3 .PDF files: (1) Instructions and Installation Guide, (2) Printable Productive Planner, (3)Linked Digital Productive Planner. Please note that THIS PRODUCT IS DIGITAL IN NATURE, NO PHYSICAL PRODUCT WILL BE SHIPPED.",
+
   },
   {
-    id: 5,
+    id: 4,
     name: "READING PLANNER",
     price: 200,
     instock: 20,
     description:
       "This journal is designed with the book lover in mind. Keep track to your favorite books and quotes, set reading goals and level up your reading challenges!",
     imgSrc: "./products_page/reading_planner.png",
+    productivity1:"./products_page/reading (1).png",
+    productivity2:"./products_page/reading (2).png",
+    productivity3:"./products_page/reading (3).png",
+    detail:
+    "With 12 creative templates, this journal is designed to set reading goals, keep track of your favorite books, make note of what books to read, and level up your reading with our book challenges. Once purchased, you'll receive: An email with a link to download your purchase. This will include 1 .ZIP file which includes 3 .PDF files: (1) Instructions and Installation Guide, (2) Printable Productive Planner, (3)Linked Digital Productive Planner. Please note that THIS PRODUCT IS DIGITAL IN NATURE, NO PHYSICAL PRODUCT WILL BE SHIPPED.",
   },
+  
 ];
