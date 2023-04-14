5e7e5ce802f5a0cb7ce14ad999a41046e7655886
muningluning <121824167+muningluning@users.noreply.github.com>
2023-04-14 16:46:55 +0800

    Add files via upload
    update_04142023_index and digital products

--- products.js
+++ products.js
@@ -1,57 +1,56 @@
 const products = [
   {
     id: 0,
-    name: "Product 1",
-    price: 100,
+    name: "90-DAY DAILY GOAL PLANNER",
+    price: 1300,
     instock: 20,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
-
-    imgSrc: "./main_assets/sample products.jpg",
+      "This planner is designed to help you accelerate reaching your goals in a 3-month actionable print!",
+    imgSrc: "./products_page/dailygoal_planner.png",
   },
   {
     id: 1,
-    name: "Product 2",
-    price: 150,
+    name: "FINANCIAL PLANNER",
+    price: 700,
     instock: 20,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
-    imgSrc: "./main_assets/digital-planner.jpg",
+      "This digital planner is your comrade in arms in organizing and taking control of your money, from recurring bills to paying down debt!",
+    imgSrc: "./products_page/financial_planner.png",
   },
   {
     id: 2,
-    name: "Product 3",
+    name: "FITNESS PLANNER",
     price: 200,
     instock: 20,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
-    imgSrc: "./main_assets/sample products.jpg",
+      "This planner will help you level up your fitness game as you organize your fitness goals, perfect that workout schedules and track your progress and measurements!",
+    imgSrc: "./products_page/fitness_planner.png",
   },
   {
     id: 3,
-    name: "Product 4",
+    name: "HAPPY HOME PLANNER",
     price: 200,
     instock: 20,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
-    imgSrc: "./main_assets/digital-planner.jpg",
+      "This digital planner is perfect in keeping your cleaning schedules, household meals, and household bills organized!",
+    imgSrc: "./products_page/home_planner.png",
   },
   {
     id: 4,
-    name: "Product 5",
+    name: "NUTRITION PLANNER",
     price: 200,
     instock: 20,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
-    imgSrc: "./main_assets/sample products.jpg",
+      "From planning your weekly meals to logging your favorite recipes, we're helping you stay organized and maintain a healthy and balanced diet!",
+    imgSrc: "./products_page/nutrition_planner.png",
   },
   {
     id: 5,
-    name: "Product 6",
+    name: "READING PLANNER",
     price: 200,
     instock: 20,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
-    imgSrc: "./main_assets/digital-planner.jpg",
+      "This journal is designed with the book lover in mind. Keep track to your favorite books and quotes, set reading goals and level up your reading challenges!",
+    imgSrc: "./products_page/reading_planner.png",
   },
 ];
